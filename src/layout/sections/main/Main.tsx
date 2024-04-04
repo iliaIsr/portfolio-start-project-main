9b156290310ab2bce7fb6871338dd598c5657b99
iliaIsr <ilil.sbl91@gmail.com>
2024-04-04 10:48:08 +0300

    add photo, main

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi there</span>
+                    <Name>I am Ilya Sobolevski</Name>
+                    <MainTitle>A Web Devoloper.</MainTitle>
+                </div>
+
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+    min-height: 100vh;
+    background-color: #fcfc5d;
+`
+export const Photo = styled.img`
+    width: 350px;
+    height: 430px;
+    object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+
+`
+const Name = styled.h2`
+
+`
