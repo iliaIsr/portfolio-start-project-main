aaa9b6a44aa77bd02c943646900ab4aa9c49d2ce
iliaIsr <ilil.sbl91@gmail.com>
2024-04-04 18:52:46 +0300

    skill, works

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css';
-import styled from 'styled-components';
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
+//import React from "react";
+import {Skills} from "./layout/sections/skills/Skills";
+import {Works} from "./layout/sections/works/Works";
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <div className="App">
             <Header/>
             <Main/>
+            <Skills/>
+            <Works/>
 
         </div>
     );
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,25 +2,15 @@ import React from 'react';
 import styled from "styled-components";
 
 
-export const Menu = () => {
+export const Menu = (props: {menuItems: Array<string>}) => {
     return (
         <StyledMeny>
             <ul>
-                <li>
-                    <a href="">Home</a>
-                </li>
-                <li>
-                    <a href="">Skills</a>
-                </li>
-                <li>
-                    <a href="">Works</a>
-                </li>
-                <li>
-                    <a href="">Testymony</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
+                {props.menuItems.map((item: string, index)=>{
+                    return <li key={index}>
+                        <a href="">{item}</a>
+                    </li>
+                    })}
             </ul>
         </StyledMeny>
     );
@@ -30,5 +20,6 @@ const StyledMeny=styled.nav`
 ul {
     display: flex;
     gap: 30px;
+    justify-content: center;
 }
 `
