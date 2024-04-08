5592e0de40d718276fbf8a3d0573302a0a4c8a7a
iliaIsr <ilil.sbl91@gmail.com>
2024-04-08 16:47:16 +0300

    add footer, contacts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import {Main} from "./layout/sections/main/Main";
 import {Skills} from "./layout/sections/skills/Skills";
 import {Works} from "./layout/sections/works/Works";
 import {Testimony} from "./layout/sections/testimony/Testimony";
+import {Contacts} from "./layout/sections/contacts/Contacts";
+import {Slogan} from "./layout/sections/slogan/Slogan";
+import {Footer} from "./layout/footer/Footer";
+
 
 
 function App() {
@@ -16,6 +20,10 @@ function App() {
             <Skills/>
             <Works/>
             <Testimony/>
+            <Contacts/>
+            <Slogan/>
+            <Footer/>
+
 
         </div>
     );
