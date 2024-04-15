997ed411abcb657323877954b21a6408530e5747
iliaIsr <ilil.sbl91@gmail.com>
2024-04-15 15:35:32 +0300

    add main
    problems with fonts in skills section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,30 +2,48 @@ import React from 'react';
 import styled from "styled-components";
 import photo from '../../../assets/images/photo.webp'
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"}>
+            <Container>
+            <FlexWrapper align={"center"} justify={"space-between"}>
                 <div>
-                    <span>Hi there</span>
-                    <Name>I am Ilya Sobolevski</Name>
+                    <SmallText>Hi there</SmallText>
+                    <Name>I am <span>Ilya Sobolevski</span></Name>
                     <MainTitle>A Web Devoloper.</MainTitle>
                 </div>
-
-                <Photo src={photo} alt=""/>
+                <PhotoWrapper>
+                    <Photo src={photo} alt=""/>
+                </PhotoWrapper>
             </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
 
 const StyledMain = styled.section`
     min-height: 100vh;
-    background-color: yellow;
-    
-    
-   
+    display: flex;
+`
+const PhotoWrapper=styled.div`
+    position: relative;
+    z-index: 0;
+
+    &::before {
+        content: "";
+        width: 360px;
+        height: 470px;
+        border: 5px solid ${theme.colors.accent};
+        position: absolute;
+        top: -24px;
+        left: 24px;
+        z-index: -1;
+
+    }
 `
 export const Photo = styled.img`
     width: 350px;
@@ -34,8 +52,42 @@ export const Photo = styled.img`
 `
 
 const MainTitle = styled.h1`
+    font-size: 27px;
+    font-weight: 400;
+    
 
 `
 const Name = styled.h2`
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 50px;
+    font-weight: 700;
+    letter-spacing: 0.05em;
+    margin: 10px 0;
+
+    span {
+        position: relative;
+        z-index: 0;
+        
+        &::before {
+            content: "";
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color:${theme.colors.accent};
+            
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+
+        }
+    }
+
+
 
 `
+
+const SmallText=styled.h2`
+    font-size: 14px;
+    font-weight: 400;
+`
+
