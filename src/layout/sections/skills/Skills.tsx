b95803ba0e81ac4ac7e2fdaf2479f36ab64e2cb4
iliaIsr <ilil.sbl91@gmail.com>
2024-04-15 16:26:05 +0300

    add styled skill

---
+++ src/layout/sections/skills/Skills.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {SectionTitle} from "../../../components/SectionTitle";
+import {Skill} from "./skill/Skill";
+import {Container} from "../../../components/Container";
+
+export const Skills = () => {
+    return (
+        <StyledSkills >
+            <Container>
+            <SectionTitle>My Skills</SectionTitle>
+            <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+                <Skill iconId={"codeSvg"} title={"html5"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"css"} title={"css"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"react"} title={"react"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"figma"} title={"figma"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"typescript"} title={"typescript"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                <Skill iconId={"sc"} title={"sc"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+            </FlexWrapper>
+            </Container>
+        </StyledSkills>
+    );
+};
+
+const StyledSkills=styled.section`
+    
+    
+`
