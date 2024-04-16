2ae8293044f53aa5b1ae2a64ed95a96c7244a901
iliaIsr <ilil.sbl91@gmail.com>
2024-04-17 00:30:58 +0300

    TabMenu, works

--- src/layout/sections/skills/skill/Skill.tsx
+++ src/layout/sections/skills/skill/Skill.tsx
@@ -3,7 +3,7 @@ import {Icon} from "../../../../components/icon/Icon";
 import styled from "styled-components";
 import {FlexWrapper} from "../../../../components/FlexWrapper";
 
-type SkillPropsType={
+type SkillPropsType = {
     iconId: string;
     title: string
     description: string
@@ -25,50 +25,41 @@ export const Skill = (props: SkillPropsType) => {
         </StyledSkill>
     );
 };
-const StyledSkill=styled.div`
+const StyledSkill = styled.div`
     width: 380px;
     padding: 62px 20px 40px;
 `
 
-const IconWrapper=styled.div`
-position: relative;
-    
-    &::before{
+const IconWrapper = styled.div`
+    position: relative;
+
+    &::before {
         content: "";
         display: inline-block;
         height: 80px;
         width: 80px;
         background: rgba(255, 255, 255, 0.1);
         transform: rotate(45deg) translate(-50%, -50%);
-        
+
         position: absolute;
         left: 50%;
         top: 50%;
         transform-origin: top left;
 
 
-
-
     }
 `
 
-const SkillTitle=styled.h3`
+const SkillTitle = styled.h3`
     margin: 70px 0 15px;
-    font-family: 'Josefin Sans', sans-serif;
-    font-size: 16px;
-    font-weight: 700;
-    
-    letter-spacing: 1px;
     text-transform: uppercase;
 
 
 `
-const SkillText=styled.p`
+const SkillText = styled.p`
     text-align: center;
-    font-size: 14px;
-    font-weight: 400;
     line-height: 1.4;
-   
+
 
 
 
