1eaa67b044e1db5d894620f5a877b6aa5b7df968
iliaIsr <ilil.sbl91@gmail.com>
2024-04-17 14:32:15 +0300

    contacts

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {IconWrapper} from "../skills/skill/Skill";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
             <FlexWrapper direction={"column"} align={"center"}>
+                <IconWrapper>
                 <Icon iconId={"quote"}/>
+                </IconWrapper>
                 <Slider/>
             </FlexWrapper>
         </StyledTestimony>
@@ -21,4 +24,8 @@ const StyledTestimony = styled.section`
     
     background-color: #becaf8;
     min-height: 50vh;
+    
+    ${IconWrapper} {
+        margin: 40px 0 72px;
+    }
 `
