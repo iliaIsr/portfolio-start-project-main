1474fa2917f4e9d2c49a6b7049f4034dc31fc26b
iliaIsr <ilil.sbl91@gmail.com>
2024-04-21 17:06:06 +0300

    MobileMenu

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -4,6 +4,7 @@ import {Logo} from "../../components/logo/Logo";
 import {Container} from "../../components/Container";
 import {FlexWrapper} from "../../components/FlexWrapper";
 import {HeaderMenu} from "./headerMenu/HeaderMenu";
+import {MobileMenu} from "./mobileMenu/MobileMenu";
 
 
 
@@ -15,6 +16,8 @@ export const Header = () => {
                 <FlexWrapper justify={"space-between"} align={"center"}>
                     <Logo/>
                     <HeaderMenu menuItems={items}/>
+                    <MobileMenu menuItems={items}/>
+
                 </FlexWrapper>
             </Container>
         </StyledHeader>
