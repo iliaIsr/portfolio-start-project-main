8259e6febe49baa6f798ef7e21f3c0a235d3be24
iliaIsr <ilil.sbl91@gmail.com>
2025-04-11 15:50:30 +0300

    add work

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -18,11 +18,14 @@ export const TabMenu = (props: {menuItems: Array<string>}) => {
 };
 
 const StyleTabdMenu=styled.nav`
-    margin-bottom: 40px;
+    
 ul {
     display: flex;
-    gap: 20px;
-    justify-content: center;
+    justify-content: space-between;
+    max-width: 352px;
+    width: 100%;
+    border:solid 2px red;
+    margin: 0 auto 40px;
 }
     
 `
